44ec533e74d0f9473543eb63b8569404ff17dd9d
GustavoYM01 <gumatimoto@hotmail.com>
2022-07-14 18:46:52 -0300

    css_react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+import { useState } from "react";
+import Title from "./components/Title";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Teste");
+
+  const redTitle = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* CSS global */}
+      <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent />
+      <p>Parágrafo do App.js</p>
+      {/* Inline CSS */}
+      <p
+        style={{
+          backgroundColor: "black",
+          color: "#fff",
+          padding: "10px",
+          borderTop: "2px solid red",
+          borderBottom: "2px solid red",
+        }}
+      >
+        Elemento estilizado com CSS inline
+      </p>
+      {/* CSS inline dinâmico */}
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Teste" ? { color: "green", backgroundColor: "#000" } : null
+        }
+      >
+        CSS dinâmico 2
+      </h2>
+      <h2 className={redTitle ? "red-title" : "title"}>Título com classe dinâmica</h2>
+      {/* CSS Modules */}
+      <Title />
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p className="my-component-paragraph">Parágrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
